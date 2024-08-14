73b1313e2ef3dbd5a23f151d96bf6c971d611f0e
Rixels <mikey.drozd@gmail.com>
2024-08-14 16:59:50 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+    let string = n.toString();
+    let out = '';
+    let i;
+    for (i = 0; i < string.length; i = i + 1) {
+       out = `${string[i]}${out}`;
+    }
+    return parseInt(out);
+  }
